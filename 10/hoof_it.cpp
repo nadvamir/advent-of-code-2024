13b58d1b3065e5fb0914660d2896691ac5a292a8
Maksim Solovjov <himaxx2@gmail.com>
2024-12-29 20:59:12 +0000

    Day 10: Solve Hoof it

---
+++ 10/hoof_it.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <cassert>
+#include <algorithm>
+#include <ranges>
+#include <set>
+#include <utility>
+#include <optional>
+
+using Peaks = std::optional<std::set<std::pair<int64_t, int64_t>>>;
+
+Peaks map_trails(const auto& input, auto i, auto j, auto& reachability) {
+    if (reachability[i][j]) return reachability[i][j];
+    if (input[i][j] == '9') {
+        reachability[i][j] = Peaks{{{i, j}}};
+        return reachability[i][j];
+    }
+
+    auto out_of_bounds = [&input](auto i, auto j) { return i < 0 || j < 0 || i >= input.size() || j >= input[i].size(); };
+    char next = input[i][j] + 1;
+    reachability[i][j] = std::set<std::pair<int64_t, int64_t>>{};
+
+    constexpr std::array<std::pair<int, int>, 4> directions{{{-1, 0}, {1, 0}, {0, -1}, {0, 1}}};
+    for (auto [di, dj] : directions) {
+        auto ni = i + di, nj = j + dj;
+        if (out_of_bounds(ni, nj)) continue;
+        if (input[ni][nj] != next) continue;
+        auto reachable = map_trails(input, ni, nj, reachability);
+        if (reachable) {
+            reachability[i][j]->insert(begin(*reachable), end(*reachable));
+        }
+    }
+    return reachability[i][j];
+}
+
+int64_t solve(const std::vector<std::string>& input) {
+    std::vector<std::vector<Peaks>> reachability(input.size(), std::vector(input[0].size(), Peaks{}));
+    int64_t result = 0;
+    for (int64_t i = 0; i < input.size(); ++i) {
+        for (int64_t j = 0; j < input[i].size(); ++j) {
+            if (input[i][j] == '0') {
+                result += map_trails(input, i, j, reachability)->size();
+            }
+        }
+    }
+    return result;
+}
+//------------------------------------------------------------------------------
+#define TEST(a) { std::cout << #a << " == " << (a) << std::endl; assert((a)); }
+
+std::vector<std::string> read_input(std::istream& is) {
+    std::vector<std::string> input;
+    std::string line;
+    while (getline(is, line)) {
+        input.push_back(line);
+    }
+    return input;
+}
+
+void test() {
+    const std::string input_text = R"_(89010123
+78121874
+87430965
+96549874
+45678903
+32019012
+01329801
+10456732)_";
+    std::istringstream is(input_text);
+    const auto input = read_input(is);
+    TEST(solve(input) == 36);
+}
+
+void run() {
+    const auto input = read_input(std::cin);
+
+    using namespace std::chrono;
+    const auto start_ts = high_resolution_clock::now();
+
+    std::cout << solve(input) << "\n";
+    
+    const auto end_ts = high_resolution_clock::now();
+    std::cout << "Elapsed time: " << duration_cast<microseconds>(end_ts - start_ts).count() << "us\n";
+}
+
+int main() {
+    test();
+    run();
+}
+
